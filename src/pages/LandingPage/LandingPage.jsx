c0096fa4039eae4f5f6ff3d979917f5e963a0e37
Brijgopal Bharadwaj <brijgopalbharadwaj1999@gmail.com>
2020-10-22 20:01:10 +0530

    Added Routing and Redux to the application
    This PR incorporates routing and redux in the application, and a brief functional demo of
    the code is presented in the 'LandingPage' component, that showcases the expected usage.
    The code being submitted has been written using the latest recommendations in mind,
    like using react hooks for Routing and redux, as well as conforming to ESLint.

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { setPlayers } from "../../redux/actions/Players Actions";
 import "./LandingPage.css";
 
 const LandingPage = () => {
+    const [player1, setPlayer1] = useState("");
+    const [player2, setPlayer2] = useState("");
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log(player1, player2);
+        dispatch(setPlayers(player1, player2, history));
+    };
+
     return (
         <div>
             IEEE-VIT&apos;s Tic Tac Toe!
---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/Root Reducer";
+
+const initialState = {};
+const middleware = [thunk];
+const devTools =
+    process.env.NODE_ENV === "production"
+        ? applyMiddleware(...middleware)
+        : compose(
+              applyMiddleware(...middleware),
+              window.__REDUX_DEVTOOLS_EXTENSION__ &&
+                  window.__REDUX_DEVTOOLS_EXTENSION__()
+          );
+
+const store = createStore(rootReducer, initialState, devTools);
+
+export default store;
