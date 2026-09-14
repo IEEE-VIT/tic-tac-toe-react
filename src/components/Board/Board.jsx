5bd10fbc5f7b92d2dabed604d62edaf637f2cae0
Guru Priya <gurupriya.mj2026@vitstudent.ac.in>
2026-09-14 16:13:17 +0530

    Restart Game

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -87,6 +87,14 @@ const Board = () => {
     return (
         <div className="board">
             <div className="board__current">Current turn: {turn}</div>
+
+<button
+    type="button"
+    className="board__restart-btn"
+    onClick={resetBoard}
+>
+    Restart
+</button>
             <div className="board__square-grp">
                 {board.map((val, index) => (
                     <Square
