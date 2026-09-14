d10953cbcf84adff7ea60550c3452fc6754c3ec9
ahu3024 <ishanahuja468@gmail.com>
2026-09-14 16:39:29 +0530

    Fixed scoreboard not rendering

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,15 +5,20 @@ import rootReducer from "./reducers/Root Reducer";
 
 const initialState = {};
 const middleware = [thunk];
-const devTools =
-    process.env.NODE_ENV === "production"
-        ? applyMiddleware(...middleware)
-        : compose(
-              applyMiddleware(...middleware),
-              window.__REDUX_DEVTOOLS_EXTENSION__ &&
-                  window.__REDUX_DEVTOOLS_EXTENSION__()
-          );
 
-const store = createStore(rootReducer, initialState, devTools);
+// Use the DevTools extension's compose when it's installed, otherwise fall
+// back to plain redux compose. `window.__REDUX_DEVTOOLS_EXTENSION__ && ...()`
+// crashed the store on creation whenever the extension wasn't installed,
+// since it passed `undefined` as an enhancer to `compose`.
+const composeEnhancers =
+    (process.env.NODE_ENV !== "production" &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(...middleware))
+);
 
 export default store;
