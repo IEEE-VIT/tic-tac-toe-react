b81def989a3768b63bff0017ce81eb574aed3e83
Guru Priya <gurupriya.mj2026@vitstudent.ac.in>
2026-09-14 15:59:13 +0530

    Its a Draw!

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -50,27 +50,40 @@ const Board = () => {
     });
 
     const winPrompt = winner ? (
-        <div className="board__winner">
-            <div className="board__winner--msg">{winner} has won the game!</div>
-            <div className="board__winner--choice">
-                Would you like to have another go?
-                <button
-                    type="button"
-                    className="board__btn"
-                    onClick={() => resetBoard()}
-                >
-                    Yes
-                </button>
-                <button
-                    type="button"
-                    className="board__btn"
-                    onClick={() => history.push("/")}
-                >
-                    No
-                </button>
-            </div>
+    <div
+        className={
+            winner === "No one"
+                ? "board__winner board__winner--draw"
+                : "board__winner"
+        }
+    >
+        <div className="board__winner--msg">
+            {winner === "No one"
+                ? "It's a Draw!"
+                : `${winner} has won the game!`}
         </div>
-    ) : null;
+
+        <div className="board__winner--choice">
+            Would you like to have another go?
+
+            <button
+                type="button"
+                className="board__btn"
+                onClick={() => resetBoard()}
+            >
+                Yes
+            </button>
+
+            <button
+                type="button"
+                className="board__btn"
+                onClick={() => history.push("/")}
+            >
+                No
+            </button>
+        </div>
+    </div>
+) : null;
     return (
         <div className="board">
             <div className="board__current">Current turn: {turn}</div>
